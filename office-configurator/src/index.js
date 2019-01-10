bc6ce3a4debbea0df561c47c214551277c1a57ec
Richard Franklin <richard.franklin@officedepot.eu>
2019-01-10 11:31:27 +0000

    Adding Redux

--- office-configurator/src/index.js
+++ office-configurator/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+
+import reducers from './reducers';
+
 import './index.scss';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<p>Test Content here</p>
+	</Provider>
+	, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
