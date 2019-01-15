86e8a8cb86df4b9d4d6ade9d848881c21ded9947
Richard Franklin <richard.franklin@officedepot.eu>
2019-01-15 11:21:49 +0000

    using endpoint from back-end

--- office-configurator/src/actions/fetchPage.js
+++ office-configurator/src/actions/fetchPage.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_PAGE = 'fetch_page';
-const ROOT_URL = 'http://localhost:4000/homeoffice';
+const ROOT_URL = 'http://10.133.92.13:8088/search/office/popular';
 
 export default function fetchPage() {
     const request = axios.get(ROOT_URL);
