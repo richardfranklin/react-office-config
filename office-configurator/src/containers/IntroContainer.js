0437b51afdae1def666156d6e31764319f9b23b9
Mark Bensley <Mark.Bensley@officedepot.com>
2019-01-15 11:16:55 +0000

    Class changed to className

--- office-configurator/src/containers/IntroContainer.js
+++ office-configurator/src/containers/IntroContainer.js
@@ -6,18 +6,18 @@ class IntroContainer extends Component {
     render() {
 		if (typeof this.props.name === 'string') {
 			return (
-				<header class="main__header">
-					<div class="row">
-						<div class="col-8">
+				<header className="main__header">
+					<div className="row">
+						<div className="col-8">
 							<h1>{this.props.name}</h1>
 							<p>{this.props.description}</p>
 							{/* <p>{this.props.numberOfEmployees}</p> */}
 						</div>
 
-						<div class="col-4 main__header-right">
-						<button class="btn btn-primary ripple add-to-cart-btn" type="submit" id="addToBasketTop" title="Add to basket" data-add-to-cart="true">
-							<i class="add-to-cart-btn__icon fa fa-shopping-cart" aria-hidden="true"></i>
-							<span class="product-add-to-cart-panel__text">Add to basket</span>
+						<div className="col-4 main__header-right">
+						<button className="btn btn-primary ripple add-to-cart-btn" type="submit" id="addToBasketTop" title="Add to basket" data-add-to-cart="true">
+							<i className="add-to-cart-btn__icon fa fa-shopping-cart" aria-hidden="true"></i>
+							<span className="product-add-to-cart-panel__text">Add to basket</span>
 						</button>
 						</div>
 					</div>
